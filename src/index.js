399d2973ed95f448afa6dbc8c1b2675c52b90d42
Anna Sadovaia <lady.sadovaya@gmail.com>
2026-05-06 18:56:41 +0500

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  const result = [];
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  const newResult = matrix.map((row, index) => {
+    // [...row] создает копию, чтобы reverse() не ломал исходную матрицу
+    return index % 2 !== 0 ? [...row].reverse() : row;
+  });
+
+  return result.concat(...newResult);
 };
